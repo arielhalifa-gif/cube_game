48572b8e46336a4a9f646cc0b9afb935ea8424cf
Ariel <arielhalifa@gmail.com>
2025-10-23 16:57:29 +0300

    modif

---
+++ utils.py
@@ -0,0 +1,28 @@
+import random
+def roll_two_d6() -> tuple[int, int]:
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    return dice1, dice2
+
+def is_bust(score: int) -> bool:
+    if score > 100:
+        return True
+    else: return False
+
+def is_exact_100(score: int) -> bool:
+    if score == 100:
+        return True
+    else: return False
+def closer_to_target(a: int, b: int, target: int = 100) -> int | None:
+    p1 = target - a
+    p2 = target - b
+    if p1 > p2:
+        return 2
+    elif p2 > p1:
+        return 1
+    else:
+        return
+
+
+def tie_breaker(roller) -> int:
+    
